b0f75e7f9d4518524610f74da8589956715803a0
kareem-sh <karimshaheen467@gmail.com>
2025-12-07 14:43:38 +0300

    sql file read added

--- main.py
+++ main.py
@@ -2,7 +2,19 @@ from antlr4 import *
 from SqlLexer import SqlLexer
 import os
 
+def tokenize_from_file(filepath):
+    """Read SQL from a file and tokenize it"""
+    try:
+        with open(filepath, 'r', encoding='utf-8') as f:
+            sql_code = f.read()
+        tokenize(sql_code)
+    except FileNotFoundError:
+        print(f"Error: File '{filepath}' not found.")
+    except Exception as e:
+        print(f"Error reading file: {e}")
+
 def tokenize(code):
+    """Tokenize SQL code"""
     input_stream = InputStream(code)
     lexer = SqlLexer(input_stream)
     tokens = lexer.getAllTokens()
@@ -10,141 +22,10 @@ def tokenize(code):
     for token in tokens:
         print(f"Token: {lexer.symbolicNames[token.type]}, Text: {token.text}")
 
-sql = "SELECT name, age FROM users;"
-sql3 = r"SELECT 'abc\
-def' AS test;"
-sql4 = """/* comment 
-/* nested */
-*/ SELECT * FROM table1;"""
-sql5 = "SELECT 0xFF, 0b1101;"
-sql6 = """DECLARE @x INT;
-SELECT @x;"""
-sql7 = "SELECT 0xabc\
-def AS Name;"
-sql8= "SELECT * WHERE name = 'Karim'"
-sql9 = r"""
+# Example usage:
+# Read from a SQL file
+tokenize_from_file('test.sql')
 
-CREATE TABLE [dbo].[DIAGNOSIS](
-	[DIAGNOSIS_KEY'1] [int] NOT NULL,
-	[DIAGNOSIS_NAME] [nvarchar](300) NOT NULL,
-	[DIAGNOSIS_CATEGORY_KEY] [int] NULL
-	)
-
-IF NOT EXISTS (
-    SELECT 1 FROM sys.columns
-    WHERE Name = 'KEY1''S'
-      AND Object_ID = Object_ID('FACT1')
-)
-BEGIN
-		ALTER TABLE FACT1 
-        ADD [KEY1'S] INT NULL;
-END
-GO
-
-UPDATE FACT1
-SET [KEY1'S] =
-    CASE 
-        WHEN KEY2 = 4 OR KEY3 IN (1,7) THEN 1
-        WHEN KEY5 = 2 THEN 2
-        ELSE 0
-    END
-WHERE [KEY1'S] IS NULL;
-GO
-
-IF NOT EXISTS (
-    SELECT 1 FROM sys.columns 
-    WHERE Name = 'HASH2' 
-      AND Object_ID = Object_ID('FACT_2')
-)
-BEGIN
-    ALTER TABLE FACT_2 
-    ADD HASH2 BIGINT NULL;
-END
-GO
-
-IF NOT EXISTS (
-    SELECT 1 FROM sys.columns 
-    WHERE Name = 'DIAGNOSIS_KEY''1' 
-      AND Object_ID = Object_ID('FACT_2')
-)
-BEGIN
-    ALTER TABLE FACT_2 
-    ADD [DIAGNOSIS_KEY'1] INT NULL;
-END
-GO
-
-
-DECLARE @ErrorMessage         NVARCHAR(4000)          
-DECLARE @ErrorState           INT          
-DECLARE @ErrorSeverity        INT 
-
-/* DROP CONSTARINTS */
-declare @sql_drop_constarints nvarchar(max)
-set @sql_drop_constarints = ''
-
-SELECT @sql_drop_constarints += N'
-ALTER TABLE ' + QUOTENAME(OBJECT_SCHEMA_NAME(parent_object_id))
-    + '.' + QUOTENAME(OBJECT_NAME(parent_object_id)) + 
-    ' DROP CONSTRAINT ' + QUOTENAME(name) + ';' 
-FROM sys.foreign_keys;
-
-BEGIN TRY
-EXEC sp_executesql @sql_drop_constarints;
-END TRY
-BEGIN CATCH
-
-    --Obtain the error message, severity and state      
-    SELECT @ErrorMessage = ERROR_MESSAGE(), @ErrorSeverity = ERROR_SEVERITY(), @ErrorState = ERROR_STATE()      
-    --RAISERROR (@ErrorMessage, @ErrorSeverity, @ErrorState)         
-END CATCH
-
-
-/* DROP ALL TABLES EXECPT 
-/* DROP ALL TABLES EXECPT 
-*/
-*/
-DECLARE @sql_drop_tables NVARCHAR(max)=''
-
-SELECT @sql_drop_tables += ' Drop table ' + QUOTENAME(TABLE_SCHEMA) + '.'+ QUOTENAME(TABLE_NAME) + '; '
-FROM   INFORMATION_SCHEMA.TABLES
-WHERE  TABLE_TYPE = 'BASE TABLE'
-AND TABLE_NAME NOT IN ('FACT1', 'fact2', 'Fact3' )
-
-Exec Sp_executesql @sql_drop_tables
-
-BEGIN TRY
-EXEC sp_executesql @sql_drop_tables;
-END TRY
-BEGIN CATCH
-    --Obtain the error message, severity and state      
-    SELECT @ErrorMessage = ERROR_MESSAGE(), @ErrorSeverity = ERROR_SEVERITY(), @ErrorState = ERROR_STATE()      
-    --RAISERROR (@ErrorMessage, @ErrorSeverity, @ErrorState)         
-END CATCH
-"""
-
-sql10="""
-IF NOT EXISTS (
-    SELECT 1 FROM sys.columns
-    WHERE Name = 'KEY1''S'
-      AND Object_ID = Object_ID('FACT1')
-)
-BEGIN
-		ALTER TABLE FACT1 
-        ADD [KEY1'S] INT NULL;
-END
-GO
-"""
-
-sql11="""
-IF NOT EXISTS (
-    SELECT 1 FROM sys.columns 
-    WHERE Name = 'DIAGNOSIS_KEY''1' 
-      AND Object_ID = Object_ID('FACT_2')
-)
-BEGIN
-    ALTER TABLE FACT_2 
-    ADD [DIAGNOSIS_KEY'1] INT NULL;
-END
-GO
-"""
-tokenize(sql4)
+# Or tokenize a direct string
+sql_string = "SELECT * FROM users;"
+tokenize(sql_string)
